615e936dcd5056f8a404357b92c8ebfc6f64242a
DMigdal <migdal.daniel@gmail.com>
2016-07-29 12:48:53 +0200

    29 june

--- js/navigation.js
+++ js/navigation.js
@@ -2,10 +2,6 @@ $(document).ready(function () {
 	$(window).scroll(function() {
                             if ($(this).scrollTop() > 150) {
                                 $( ".navbar-defaultl" ).css({'background':'grey', 'opacity':'0.7', 'font-size':'0.8em'});
-                            } else
-                            {
-                               
-                                //$( ".navbar-deafultl" ).toggleClass();
                             } 
 
                             
